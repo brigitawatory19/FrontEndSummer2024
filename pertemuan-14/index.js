0680e3b71646d55534be9ae445e1dea47691cb5b
brigitawatory19 <watorybrigita19@gmail.com>
2024-06-21 09:00:15 +0800

    Update index.js

--- pertemuan-14/index.js
+++ pertemuan-14/index.js
@@ -1,3 +1,7 @@
+// working on remote repository
 console.log("Hello Github and Github");
 console.log("Coba Lagi")
 console.log("Ini Dirubah Langsung dari Github")
+console.log("Test git pull")
+console.log("Testing marge conflict dari lokal")
+console.log("Test marge conflic")
